4c27c0258dc4a6e3ed9fac3c0c86f5492fed674c
kerobs <gjerobs@gmail.com>
2026-09-16 11:25:28 +0800

    relative url to root-relative url

--- src/App.tsx
+++ src/App.tsx
@@ -167,7 +167,7 @@ function Hero() {
     >
       <div className="absolute inset-0">
         <img
-          src="img/hero-bg.jpg"
+          src="/img/hero-bg.jpg"
           alt="Couple walking on a beach in the Philippines"
           className="w-full h-full object-cover object-center"
         />
@@ -348,7 +348,7 @@ function OurStory() {
           <div className="relative">
             <div className="absolute -top-4 -left-4 w-full h-full border border-[#d4b896]/40" />
             <img
-              src="img/our-story.jpg"
+              src="/img/our-story.jpg"
               alt="Couple walking through lush greenery"
               className="w-full h-[480px] object-cover relative z-10"
             />
